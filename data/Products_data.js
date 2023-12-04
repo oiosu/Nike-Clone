5f38b80730569eae881501db717e89f401b672cc
Doit <duhee7524@naver.com>
2023-12-04 14:31:19 +0900

    chore : data폴더 추가
    chore : data폴더 추가

---
+++ data/Products_data.js
@@ -0,0 +1,21 @@
+const clothesData=[
+    {
+        "index": 1,
+        "src":"https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/8e612413-a0a2-4c16-884e-c1e9f3dfaed7/%EC%8A%A4%ED%8F%AC%EC%B8%A0%EC%9B%A8%EC%96%B4-%EC%BB%AC%EB%A0%89%EC%85%98-%EC%97%AC%EC%84%B1-%ED%95%98%EC%9D%B4-%ED%8C%8C%EC%9D%BC-%ED%94%8C%EB%A6%AC%EC%8A%A4-%ED%95%98%ED%94%84%EC%A7%91-%ED%83%91-pIOgPBLx.png",
+        "alt":"",
+        "name": "나이키 스포츠웨어 피닉스 플리스",
+        "info": "여성 오버-오버사이즈 풀오버 후디",
+        "price": 100000,
+        "discount":35
+    },
+    {
+        "index": 2,
+        "src":"https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/8e612413-a0a2-4c16-884e-c1e9f3dfaed7/%EC%8A%A4%ED%8F%AC%EC%B8%A0%EC%9B%A8%EC%96%B4-%EC%BB%AC%EB%A0%89%EC%85%98-%EC%97%AC%EC%84%B1-%ED%95%98%EC%9D%B4-%ED%8C%8C%EC%9D%BC-%ED%94%8C%EB%A6%AC%EC%8A%A4-%ED%95%98%ED%94%84%EC%A7%91-%ED%83%91-pIOgPBLx.png",
+        "alt":"",
+        "name": "나이키 스포츠웨어 피닉스 플리스",
+        "info": "여성 오버-오버사이즈 풀오버 후디",
+        "price": 100000,
+        "discount":35
+    }
+]
+export default clothesData
