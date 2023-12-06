77e59fedecac864a90e288648fdf8ea4df2aa000
SU KYUNG IM <99783474+oiosu@users.noreply.github.com>
2023-12-06 14:13:47 +0900

    feat: nav 구현 완성

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import React from "react";
 import Header from "./components/MainPage/Header";
+import Nav from "./components/MainPage/Nav";
 
 function App() {
   return (
-    <Header />
+    <div>
+      <Header />
+      <Nav />
+    </div>
   );
 }
 
