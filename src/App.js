fa4ec3ed52edb9253930189421b12a356791d8d8
SU KYUNG IM <99783474+oiosu@users.noreply.github.com>
2023-12-06 14:20:36 +0900

    navfoorer 구현 완료

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/MainPage/Header";
 import Nav from "./components/MainPage/Nav";
+import NavFooer from "./components/MainPage/NavFooer";
 
 function App() {
   return (
     <div>
       <Header />
       <Nav />
+      <NavFooer />
     </div>
   );
 }
