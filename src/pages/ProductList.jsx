abca553c9118c489f949906e816a1e07e6eb38de
Doit <duhee7524@naver.com>
2023-12-07 13:30:23 +0900

    feat: 상품 리스트 1차 개발
    상품 리스트 및 정렬 기능 구현

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Products from "../components/productList/Products.jsx";
+import clothesData from "../data/Products_data.js";
 
 const ProductList = () => {
     return (
         <>
-            <Products />
+            <Products items={clothesData}/>
         </>
     );
 };
