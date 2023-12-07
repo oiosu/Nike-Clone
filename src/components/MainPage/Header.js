aaf9c7c63516790bba43f51a74f58e6104d1c16a
SU KYUNG IM <99783474+oiosu@users.noreply.github.com>
2023-12-07 17:26:25 +0900

    feat: 고객센터 모달 기능 구현 완성

--- src/components/MainPage/Header.js
+++ src/components/MainPage/Header.js
@@ -1,5 +1,5 @@
-import React from 'react';
 import styled from "styled-components";
+import React, { useState } from 'react';
 
 const TopMenu = styled.header`
     display: flex;
@@ -14,25 +14,64 @@ const TopMenu = styled.header`
     height: 30px;
 
     ul {
-        margin: 0;
-        padding: 0;
         list-style: none;
         display: flex;
+        padding: 0;
+        margin: 0;
+    }
 
         li {
-            margin-right: 20px;
+            margin: 0 10px;
             cursor: pointer;
+
+            &:hover {
+                text-decoration: underline;
         }
     }
+
+    .modal {
+        width: 170px;
+        height: 50px;
+        margin-top: 9px;
+        background-color: #000;
+        position: absolute;
+        z-index: 1; 
+        border-radius: 30px;
+        color: #fff;
+        font-size: 15px;
+    }
+
+    .modal p {
+        text-align: center;
+
+    }
+    
 `;
 
 
 const Header = () => {
+    const [isModalVisible, setModalVisible] = useState(false);
+
+    const handleMouseEnter = () => {
+        setModalVisible(true);
+    };
+
+    const handleMouseLeave = () => {
+        setModalVisible(false);
+    };
+
     return (
         <TopMenu>
             <ul>
                 <li>매장찾기</li>
-                <li>고객센터</li>
+                <li onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+                    고객센터
+                    {isModalVisible && (
+                        <div className="modal">
+                            <p>🏄‍♂️준비중입니다!</p>
+                        </div>
+                    )}
+                </li>
                 <li>가입하기</li>
                 <li>로그인</li>
             </ul>
