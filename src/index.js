c7b1705ce1c6ca8cb596b6dbd4598d522f3af868
Doit <duhee7524@naver.com>
2023-12-08 21:35:48 +0900

    chore :기타
    chore :기타

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import ProductList from './pages/ProductList';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ProductList />
+    <App />
   </React.StrictMode>
 );
 
